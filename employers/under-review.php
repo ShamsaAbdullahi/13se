e96182b58008c6093f60061012d1ae203bdd4314
olyviah82 <77206032+olyviah82@users.noreply.github.com>
2021-04-20 14:30:39 +0300

    fourth commit

--- employers/under-review.php
+++ employers/under-review.php
@@ -5,7 +5,7 @@ session_start();
 
 //If user Not logged in then redirect them back to homepage. 
 //This is required if user tries to manually enter view-job-post.php in URL.
-if(empty($_SESSION['id_emp'])) {
+if (empty($_SESSION['id_emp'])) {
   header("Location: ../index.php");
   exit();
 }
@@ -13,17 +13,16 @@ if(empty($_SESSION['id_emp'])) {
 //Including Database Connection From db.php file to avoid rewriting in all files  
 require_once("../db.php");
 
-$sql = "SELECT * FROM apply_job_post WHERE id_emp='$_SESSION[id_emp]' AND id_user='$_GET[id]' AND id_jobpost='$_GET[id_jobpost]'";
+$sql = "SELECT * FROM applyjob WHERE id_emp='$_SESSION[id_emp]' AND id_user='$_GET[id]' AND id_jobpost='$_GET[id_jobpost]'";
 $result = $conn->query($sql);
-if($result->num_rows == 0) 
-{
+if ($result->num_rows == 0) {
   header("Location: index.php");
   exit();
 }
 
 
-$sql = "UPDATE apply_job_post SET status='2' WHERE id_emp='$_SESSION[id_emp]' AND id_user='$_GET[id]' AND id_jobpost='$_GET[id_jobpost]'";
-if($conn->query($sql) === TRUE) {
-	header("Location: job-applications.php");
-	exit();
+$sql = "UPDATE applyjob SET status='2' WHERE id_emp='$_SESSION[id_emp]' AND id_user='$_GET[id]' AND id_jobpost='$_GET[id_jobpost]'";
+if ($conn->query($sql) === TRUE) {
+  header("Location: job-applications.php");
+  exit();
 }
