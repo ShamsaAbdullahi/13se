f8fe24365cdd4cfffba2a078b097a5179fea6779
Shamsa <shamsaabdullahi02@gmail.com>
2021-04-21 15:20:23 +0300

    update

--- logout.php
+++ logout.php
@@ -1,14 +1,5 @@
 <?php
-<<<<<<< HEAD
- 
-/*
-	* session_start()  --> Required To do anything related to sessions. Best to put on top before any other code.
-	* session_unset() -->  function frees all session variables currently registered like $_Session['id_user']
-	* session_destroy() --> Destroys all data registered to a session. Basically destory all session.
-*/
-=======
 
->>>>>>> d6791d4e223c952a2f7d7ebb57e02eb2c17c19b1
 
 session_start();
 session_unset();
